5186dfef81f6c2b470e8570eccbbd364f7aed7cb
lingress <44732833+lingress@users.noreply.github.com>
2019-07-17 12:20:35 -0400

    Update Init Script to include documentation

--- InitScript.lua
+++ InitScript.lua
@@ -1,4 +1,13 @@
-﻿local genv = getgenv()
+﻿--[[
+
+	InitScript.lua
+
+	This script defines globals used by Gamer UI. Not much can be done with this, but there's
+	an example of how to use addGlobal() below.
+
+]]
+
+local genv = getgenv()
 
 genv.GamerUIBackup = {}
 local backupTbl = genv.GamerUIBackup
@@ -10,7 +19,7 @@ local function backupTbl:RestoreGenv()
 	end
 end
 
-local function addGlobal(name,val)
+local function addGlobal(name,val) --// void addGlobal(string Name, <variant> Value) Adds a value to the global environment
 	if genv[name] then
 		backupTbl[name] = genv[name]
 	end
