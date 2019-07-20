1946ff1f348670e3110b690a7ead896d6d95f584
lingress <44732833+lingress@users.noreply.github.com>
2019-07-20 08:18:16 -0400

    Add Execute and Script Hub WebSocket services

--- WebSocket.cs
+++ WebSocket.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using WebSocketSharp;
 using WebSocketSharp.Server;
+using sxlib;
+using sxlib.Specialized;
 
 /// <summary>
 /// Created a WebSocket server with Synapse X Web Socket API interoperability
@@ -28,13 +30,54 @@ namespace GamerUI
             }
         }
     }
+    internal sealed class ExecuteService : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            if (SynXLib.attached && SynXLib.isReady)
+            {
+                SynXLib.Syn.Execute(e.Data);
+                Send("OK");
+            } else
+            {
+                Send("NOT_READY");
+            }
+        }
+    }
+    internal sealed class ScriptHubService : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            if (SynXLib.attached && SynXLib.isReady)
+            {
+                SxLibBase.SynHubEntry entry = null;
+                try
+                {
+                    entry = SynXLib.GetHubEntryByName(e.Data);
+                } catch (Exception)
+                {
+                    Send("INVALID_ENTRY");
+                }
+                if (entry != null)
+                {
+                    entry.Execute();
+                    Send("OK");
+                }
+            } else
+            {
+                Send("NOT_READY");
+            }
+        }
+    }
     internal class WebSocket
     {
         public static void StartSocket()
         {
             WSServer.AddWebSocketService<EditorService>("/editor");
+            WSServer.AddWebSocketService<ExecuteService>("/execute");
+            WSServer.AddWebSocketService<ScriptHubService>("/scripthub");
             WSServer.Start();
         }
-        public static WebSocketServer WSServer = new WebSocketServer("ws://localhost:24892"); // later lmao
+        public static WebSocketServer WSServer = new WebSocketServer("ws://localhost:24892");
     }
 }
